7375097c122b88ae34ad6d7a7e30582f211373e2
nprindle <nprindle18@gmail.com>
2020-11-11 12:28:16 -0500

    Add better CPU scaling results

--- ReproducibilityChallenge/run/scripts/cpu-runs.sh
+++ ReproducibilityChallenge/run/scripts/cpu-runs.sh
@@ -8,9 +8,9 @@ done
 
 for i in {1..10}; do
 	for d in CDS1 CDS2; do
-		sbatch --job-name "memxct-multinode-${d}-1_${i}" --nodes=1 --ntasks-per-node=120 --output="memxct-multinode-${d}-1_${i}.out" cpu/multinode.sh "$d" 32 512 8
-		sbatch --job-name "memxct-multinode-${d}-2_${i}" --nodes=2 --ntasks-per-node=120 --output="memxct-multinode-${d}-2_${i}.out" cpu/multinode.sh "$d" 32 512 8
-		sbatch --job-name "memxct-multinode-${d}-4_${i}" --nodes=4 --ntasks-per-node=120 --output="memxct-multinode-${d}-4_${i}.out" cpu/multinode.sh "$d" 32 512 8
+		sbatch --job-name "memxct-multinode-${d}-1_${i}" --nodes=1 --ntasks-per-node=120 --output="memxct-multinode-${d}-1_${i}.out" cpu/multinode.sh "$d" 32 32 8
+		sbatch --job-name "memxct-multinode-${d}-2_${i}" --nodes=2 --ntasks-per-node=120 --output="memxct-multinode-${d}-2_${i}.out" cpu/multinode.sh "$d" 32 32 8
+		sbatch --job-name "memxct-multinode-${d}-4_${i}" --nodes=4 --ntasks-per-node=120 --output="memxct-multinode-${d}-4_${i}.out" cpu/multinode.sh "$d" 32 32 8
 	done
 done
 
