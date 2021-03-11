60ca7e717614990d669b33f380e0453bce3b4ef5
Nicole Prindle <nprindle18@gmail.com>
2021-03-11 15:08:55 -0500

    Reorganize repo

--- cpu/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo yum install -y tmux
-sudo yum install -y epel-release
-sudo yum install -y htop
-
-sudo yum install -y centos-release-scl
-sudo yum install -y devtoolset-9-gcc*
-
